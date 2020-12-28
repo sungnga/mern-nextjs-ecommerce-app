c3e1746a62794c7b8b82ff06dcdb3d712b5c69c9
sungnga <nga422@gmail.com>
2020-12-28 13:40:33 -0800

    Prevent, catch errors on client and server sides

--- pages/api/product.js
+++ pages/api/product.js
@@ -29,17 +29,27 @@ async function handleGetRequest(req, res) {
 async function handlePostRequest(req, res) {
 	// The payload info sent on request by the client is accessible in req.body object
 	const { name, price, description, mediaUrl } = req.body;
-	// Check to see if the value for all the input fields is provided
-	if (!name || !price || !description || !mediaUrl) {
-		// status code 422 means the user hasn't provided the necessary info
-		return res.status(422).send('Product missing one or more fields');
+	try {
+		// Check to see if the value for all the input fields is provided
+		if (!name || !price || !description || !mediaUrl) {
+			// status code 422 means the user hasn't provided the necessary info
+			return res.status(422).send('Product missing one or more fields');
+		}
+		// Create a product instance from the Product model
+		const newProduct = await new Product({
+			name,
+			price,
+			description,
+			mediaUrl
+		});
+		// Save the product to db
+		newProduct.save();
+		// status code 201 means a resource is created
+		res.status(201).json(newProduct);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Server error in creating product');
 	}
-	// Create a product instance from the Product model
-	const newProduct = await new Product({ name, price, description, mediaUrl });
-	// Save the product to db
-	newProduct.save();
-	// status code 201 means a resource is created
-	res.status(201).json(newProduct);
 }
 
 async function handleDeleteRequest(req, res) {
@@ -48,10 +58,3 @@ async function handleDeleteRequest(req, res) {
 	// status code 204 means success and no content is sent back
 	res.status(204).json({});
 }
-
-// export default async (req, res) => {
-// 	// req.query is an object
-// 	const { _id } = req.query;
-// 	const product = await Product.findOne({ _id });
-// 	res.status(200).json(product);
-// };
